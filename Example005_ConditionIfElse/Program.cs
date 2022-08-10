d4e522349b38b9b5680016f472623891836419ab
Kate <vvellspring@gmail.com>
2022-08-10 23:07:27 +0300

    Add Example--5 Condition IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "катя")
+{
+    Console.WriteLine("Ура! Это Катя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
