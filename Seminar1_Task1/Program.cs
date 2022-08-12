8a5fcf54ae72fbf93d6d1e9896237eae2bc6af34
Kate <vvellspring@gmail.com>
2022-08-12 20:23:56 +0300

    Add Seminar1 Tasks

---
+++ Seminar1_Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат
+//Console.Clear();
+//Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int sqrt = number * number;
+//Console.WriteLine("Число в квадрате: " + sqrt);
+
+Console.Clear();
+//Console.WriteLine("Input first number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//int square = num2 * num2;
+//if(num1 == square)
+//Console.WriteLine("First number is a square of second number");
+//else
+//{
+//    Console.WriteLine("First number is not a square of second number");
+//}
+
+//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.Write("Input number of the day: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+{
+Console.Write("Incorrect number");
+}
+else
+{
+if (day == 1)
+{
+Console.Write("Monday");
+}
+if (day == 2)
+{
+Console.Write("Tuesday");
+}
+}
