5952f0f112e36a808e37bef6aca51f2847e8a43f
Kate <vvellspring@gmail.com>
2022-08-17 16:47:56 +0300

    Extra taks

---
+++ Extra/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+// If - else конструкция
+
+string mes1 = "", mes2 = "";
+Console.WriteLine("Введите целое число");
+int n = int.Parse(Console.ReadLine());
+
+if(n>=0) mes1 = "положительное";
+else mes1 = "отрицательное";
+if(n%2 == 0) mes2 = "четное";
+else mes2 = "нечетное";
+
+Console.WriteLine("Число {0}, {1}, {2}", n, mes1, mes2);
+Console.ReadKey();
+
+
