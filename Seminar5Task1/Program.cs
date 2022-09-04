924c66a5d8a67dfb5e6b2636a68a87e83fa3b832
Kate <vvellspring@gmail.com>
2022-09-04 19:53:35 +0300

    Задача - замена элементов массива с противоположным знаком

---
+++ Seminar5Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.Clear();
+
+// // Скопировали метод создания рандомного массива
+int[] CreateRandomArray(int N, int start, int end)
+{
+int[] RandomArray = new int[N];
+for (int i = 0; i < N; i++)
+{
+RandomArray[i] = new Random().Next(start, end + 1);
+}
+return RandomArray;
+}
+
+// // Код
+int[] myArray = CreateRandomArray(8, -10, 11);
+Console.WriteLine(String.Join(",",myArray));
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = -myArray[i];
+}
+Console.WriteLine(String.Join(",",myArray));
+
+
