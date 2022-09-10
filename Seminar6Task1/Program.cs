1510882448f80fa7153e916e23d9b30aed371a4d
Kate <vvellspring@gmail.com>
2022-09-10 19:56:00 +0300

    Разворот массива

---
+++ Seminar6Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая на вход принимает 3 числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон
+
+//Обычное решение
+// Console.Clear();
+// Console.Write("Введите 1-ую длину стороны треугольника: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите 2-ую длину стороны треугольника: ");
+// int b = int.Parse(Console.ReadLine());
+// Console.Write("Введите 3-ую длину стороны треугольника: ");
+// int c = int.Parse(Console.ReadLine());
+// if (a + b < c && a + c < b && b + c < a)
+// {
+//     Console.WriteLine("Треугольника с такими сторонами быть не может");
+// }
+// else
+// {
+//     Console.WriteLine("Треугольник с такими сторонами может быть");
+// }
+
+//Через массивы - мое решение
+// int[] triangle = new int[3]{a, b, c};
+// Console.WriteLine(String.Join(" ", triangle));
+// if(triangle[0] + triangle[1] < triangle[2] && triangle[1] + triangle[2] < triangle[0]
+// && triangle[0] + triangle[2] < triangle[1] )
+// {
+//     Console.WriteLine("Треугольника с такими сторонами быть не может");
+// }
+// else
+// {
+//     Console.WriteLine("Треугольник с такими сторонами может быть");
+// }
+
+
+// Новое решение через массивы
+Сonsole.Clear();
+Console.Write("Введите стороны треугольника через пробел: ");
+
+string[] st = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+if(IsTriangle(int.Parse(st[0]),int.Parse(st[1]),int.Parse(st[2])))
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
+
+
+
+bool IsTriangle(int a,int b,int c)
+{
+return (((a+b)>c)&&((a+c)>b)&&((b+c)>a));
+}
---
+++ Seminar6Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+int[] res = new int[10];
+while (num / 2 != 0)
+{
+    res[i] = num % 2;
+    num = num / 2;
+    i++;
+}
+Console.WriteLine(String.Join(" ", res));
+
+    // Степан: // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// Console.WriteLine("Введите число N");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int x = 1;
+// while (N > 0)
+// {
+// if (N % 2 == 0)
+// {
+// x++;
+// N = N / 2;
+// }
+// else
+// {
+// x++;
+// N = N / 2;
+// }
+// }
+
+// int [] array = new int[x];
+// int I = 0;
+// while (N > 0)
+// {
+// if (N % 2 == 0)
+// {
+// array[I] = 0;
+// I++;
+// N = N / 2;
+// }
+// else
+// {
+// array[I] = 1;
+// I++;
+// N = N / 2;156
+// }
+// }
+
+// Console.Write(array);
+
+//Alexandra Goncharova (she/her): string FuncTo2(int chislo)
+{
+if (chislo == 1)
+return "1";
+else
+return FuncTo2(chislo / 2) + (chislo % 2);
+}
